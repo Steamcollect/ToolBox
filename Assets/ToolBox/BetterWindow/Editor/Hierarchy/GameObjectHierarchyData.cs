9128afe0af8cacc624e46ebe1fe2e39dca636b97
Steamcollect <eliot.garnier.080805@gmail.com>
2025-10-31 23:55:41 +0100

    Litle fix

---
+++ Assets/ToolBox/BetterWindow/Editor/Hierarchy/GameObjectHierarchyData.cs
@@ -0,0 +1,92 @@
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolBox.BetterWindow
+{
+    [System.Serializable]
+    public class GameObjectHierarchyData
+    {
+        public Object obj;
+        GameObject go;
+
+        public Component[] components;
+
+        int instanceID;
+
+        public GameObjectHierarchyData(Object obj)
+        {
+            if (obj == null)
+                return;
+
+            this.obj = obj;
+
+            go = obj as GameObject;
+            instanceID = go.GetInstanceID();
+            components = go.GetComponents<Component>();
+        }
+
+        public void Draw(Rect selectionRect)
+        {
+            if(ToolBoxPreferences.s_DrawModeInHierarchy == ToolBoxIconDrawModeInHierarchy.Default)
+                return;
+
+            // Check if components have changed
+            Component[] comps = go.GetComponents<Component>();
+            if (components.Length != comps.Length ||
+                (comps.Length > 1 && comps[1] != components[1]))
+            {
+                components = comps;
+            }
+
+            // Draw background
+            bool isSelected = Selection.instanceIDs.Contains(instanceID);
+            Color bgColor = isSelected
+                ? new Color(0.172549f, .3647059f, .5294118f)
+                : (EditorGUIUtility.isProSkin
+                    ? new Color(0.219f, 0.219f, 0.219f)
+                    : new Color(0.76f, 0.76f, 0.76f));
+
+            // icon
+            Rect iconRect = new Rect(selectionRect.x - 1, selectionRect.y, 16, 16);
+            if(ToolBoxPreferences.s_DrawModeInHierarchy == ToolBoxIconDrawModeInHierarchy.OnRightSide)
+                    iconRect = new Rect(selectionRect.x + selectionRect.width - 17, selectionRect.y, 16, 16);
+
+            if(ToolBoxPreferences.s_DrawModeInHierarchy != ToolBoxIconDrawModeInHierarchy.OnlyFolderIcon)
+                EditorGUI.DrawRect(iconRect, bgColor);
+
+            Texture icon = null;
+            if (ToolBoxPreferences.s_DrawModeInHierarchy == ToolBoxIconDrawModeInHierarchy.OnlyFolderIcon)
+            {
+                if (components.Length <= 1)
+                {
+                    EditorGUI.DrawRect(iconRect, bgColor);
+                    icon = EditorGUIUtility.IconContent("Folder Icon").image;
+                }
+            }                
+            else
+            {
+                if (components.Length > 1)
+                    icon = EditorGUIUtility.ObjectContent(null, components[1].GetType()).image as Texture2D;
+                else
+                    icon = EditorGUIUtility.IconContent("Folder Icon").image;
+            }
+
+            if(icon != null)
+            {
+                GUI.DrawTexture(
+                    iconRect,
+                    icon,
+                    ScaleMode.ScaleToFit);
+            }
+        }
+    }
+
+    public enum ToolBoxIconDrawModeInHierarchy
+    {
+        OnGameObjectIcon,
+        OnRightSide,
+        OnlyFolderIcon,
+        Default,
+    }
+}
