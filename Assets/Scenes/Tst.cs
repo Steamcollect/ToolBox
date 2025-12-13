d691442bd53bd7ec690cd7b6c2b3d635e34ea7fa
Steamcollect <eliot.garnier.080805@gmail.com>
2025-12-13 19:08:57 +0100

    wip

--- Assets/Scenes/Tst.cs
+++ Assets/Scenes/Tst.cs
@@ -1,18 +1,9 @@
 using MVsToolkit.Demo;
 using MVsToolkit.Dev;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Tst : MVsToolkitInspectorDemo
+public class Tst : MonoBehaviour
 {
-    // Tout par défaut : Local, Default, White
-    [Handle]
-    public Vector3 pointD;
-
-    // Handle local, forme Sphere, couleur Rouge
-    [Handle(Space.Self, HandleDrawType.Sphere, ColorPreset.Red)]
-    public Vector3 pointE;
-
-    // Handle global, forme Cube, couleur Cyan
-    [Handle(Space.World, HandleDrawType.Cube, ColorPreset.Cyan)]
-    public Vector3 pointF;
+    public UnityEvent tmp;
 }
