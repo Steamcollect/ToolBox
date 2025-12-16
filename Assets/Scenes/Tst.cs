f478a1f9d5bd2ca1db8ad46040e5c8b00aab2d7c
Steamcollect <eliot.garnier.080805@gmail.com>
2025-12-16 16:13:43 +0100

    Fix for new update v0.8.0

--- Assets/Scenes/Tst.cs
+++ Assets/Scenes/Tst.cs
@@ -6,8 +6,10 @@ public class Tst : MonoBehaviour
 {
     [SerializeField, DrawInRect("DawRect")] int tst;
 
+#if UNITY_EDITOR
     public void DrawRect(Rect rect)
     {
         EditorGUI.DrawRect(rect, Color.red);
     }
+#endif
 }
