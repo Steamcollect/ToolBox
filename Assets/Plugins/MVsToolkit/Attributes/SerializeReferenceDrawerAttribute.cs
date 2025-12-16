e78d5d1d3e60418d26c29fb3ce71174d2d9ff429
Verdraks <132646355+Verdraks@users.noreply.github.com>
2025-12-16 22:34:37 +0100

    Batch rename (#2)
    * WIP
    
    * Feat: Rename Service, Rename Operation, Rename Rules ...
    
    Rename Rules Wip, implementation servie ui wip, string builder wip ...
    
    * WIP: Batch Renamer Config
    
    * Feat : Serialize Reference Drawer #1
    
    * Fix: GUI Render same as unity
    
    * Fix : Indentation Fields
    
    * Feat : Script Batch Rename ReOrder
    
    * WIP: Editor Window #1
    
    Known bugs: Text Display when editing setting, Error handling rename operation, string builder missing, delay when editing settings preview see
    
    * Fix: Better Renamer Window Implementation
    
    * Fix: Remove useless files

---
+++ Assets/Plugins/MVsToolkit/Attributes/SerializeReferenceDrawerAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace MVsToolkit.Dev
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class SerializeReferenceDrawerAttribute : PropertyAttribute{}
+}
---
+++ Assets/Plugins/MVsToolkit/BatchRename/Target/AssetTarget.cs
@@ -0,0 +1,29 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace MVsToolkit.BatchRename
+{
+    public class AssetTarget : IRenameTarget
+    {
+        public AssetTarget(string assetPath)
+        {
+            UnityObject = AssetDatabase.LoadMainAssetAtPath(assetPath);
+            Debug.Log(UnityObject.name);
+            Path = assetPath;
+        }
+
+        public string Name => UnityObject.name;
+        public string Path { get; }
+        public Object UnityObject { get; }
+
+        public void SetName(string newName)
+        {
+            AssetDatabase.RenameAsset(Path, newName);
+        }
+
+        public bool IsValidTarget()
+        {
+            return UnityObject != null;
+        }
+    }
+}
