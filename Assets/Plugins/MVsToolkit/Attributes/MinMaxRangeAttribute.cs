081503e9ce793cc6c0cae6c06e0cc9024da47462
Steamcollect <eliot.garnier.080805@gmail.com>
2025-12-26 17:05:03 +0100

    Add min max range attribute

---
+++ Assets/Plugins/MVsToolkit/Attributes/MinMaxRangeAttribute.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MinMaxRangeAttribute : PropertyAttribute
+{
+    public readonly float FMin, FMax;
+    public readonly int IMin, IMax;
+
+    public MinMaxRangeAttribute(int min, int max)
+    {
+        IMin = min;
+        IMax = max;
+
+        FMin = min;
+        FMax = max;
+    }
+
+    public MinMaxRangeAttribute(float min, float max)
+    {
+        FMin = min;
+        FMax = max;
+
+        IMin = Mathf.RoundToInt(min);
+        IMax = Mathf.RoundToInt(max);
+    }
+}
